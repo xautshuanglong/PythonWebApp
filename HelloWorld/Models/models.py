081aabbbf81c74a519b95abafb13f14ce558fc81
xjshuanglong <xjshuanglong@126.com>
2019-05-09 17:16:48 +0800

    Models testing

--- HelloWorld/Models/models.py
+++ HelloWorld/Models/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    sex = models.SmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return "{ " + self.id + ", " + self.name + " }"

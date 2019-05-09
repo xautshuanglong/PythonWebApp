6b3fd8c9254caf03bd9c83693fdb0b6cfbc34090
xjshuanglong <xjshuanglong@126.com>
2019-05-09 20:13:48 +0800

    模型列表及单个模型获取测试； 模型序列化测试。

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -14,14 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import re_path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from Views import index
-from Views import hello
+from Views import index, hello
+from Models import views
 
 urlpatterns = [
-    re_path(r'^$', index.index, name='index'),
+    re_path('^$', index.index, name='index'),
     re_path('^index/', index.index, name='index'),
     re_path('^admin/', admin.site.urls),
+    re_path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path('docs/', include_docs_urls(title='Document')),
     re_path('^hello/$', hello.hello),
+    re_path('^user/$', views.UserInfoList.as_view()),
+    re_path('^user/(?P<id>[a-zA-Z0-9_-]+)/$', views.UserInfoOne.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- HelloWorld/Models/views.py
+++ HelloWorld/Models/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import UserInfo
+from .serializers import UserInfoSerializer
+
+
+class UserInfoList(APIView):
+    def get(self, request, format=None):
+        user_list = UserInfo.objects.all()
+        user_list_serializer = UserInfoSerializer(user_list, many=True)
+        return Response(user_list_serializer.data)
+
+    def post(self, request, format=None):
+        return Response("UserInfoList")
+
+
+class UserInfoOne(APIView):
+    def get(self, request, id, format=None):
+        try:
+            response_str = ""
+            user = UserInfo.objects.get(id=id)
+            user_serializer = UserInfoSerializer(user, many=False)
+            return Response(user_serializer.data)
+        except UserInfo.DoesNotExist:
+            return Response("Not Found")
+
+    def post(self, request, id, format=None):
+        return Response("UserInfoOne")

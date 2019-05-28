767bd5d4afb73583b4d32f616d6a97bdd0452e02
xjshuanglong <xjshuanglong@126.com>
2019-05-28 14:49:56 +0800

    vue test

--- HelloWorld/Models/admin.py
+++ HelloWorld/Models/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from Models.models import UserInfo, Question, Choice
+
+admin.site.register(UserInfo)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
+
+admin.site.register(Question, QuestionAdmin)

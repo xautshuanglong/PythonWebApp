cf560de9921ab99ba2121b1cf77f2e07375f229b
xjshuanglong <xjshuanglong@126.com>
2019-06-10 17:18:45 +0800

    自己做 静态 文件路由，无视 STATIC_URL。

--- HelloWorld/HelloWorld/settings.py
+++ HelloWorld/HelloWorld/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/'
+STATIC_URL = '/x/'
 
 # The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting
 # STATIC_ROOT = 'static'
